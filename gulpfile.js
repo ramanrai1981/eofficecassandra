235109aa102c3edc5b5dec9f2b21f90ecf840a19
Raman <raman.kumar.rai@gmail.com>
2017-05-15 19:36:20 +0530

    added markto-form and displayed corresponding detail into pendingmarktouser html

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// Generated on 2017-04-27 using generator-jhipster 4.0.7
+// Generated on 2017-05-11 using generator-jhipster 4.0.7
 'use strict';
 
 var gulp = require('gulp'),
--- src/main/webapp/app/addemployee/addemployee.state.js
+++ src/main/webapp/app/addemployee/addemployee.state.js
@@ -8,11 +8,13 @@
     stateConfig.$inject = ['$stateProvider'];
 
     function stateConfig($stateProvider) {
-        $stateProvider.state('addemployee', {
-            parent: 'app',
+        $stateProvider
+        .state('addemployee', {
+            parent: 'entity',
             url: '/addemployee',
             data: {
-                authorities: []
+                authorities: ['ROLE_USER'],
+                pageTitle: 'eofficeApp.addemployees.home.title'
             },
             views: {
                 'content@': {
@@ -22,11 +24,54 @@
                 }
             },
             resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
                     $translatePartialLoader.addPart('addemployee');
+                    $translatePartialLoader.addPart('global');
                     return $translate.refresh();
                 }]
             }
-        });
+        })
+
+        .state('addemployee.new', {
+            parent: 'addemployee',
+            url: '/new',
+            data: {
+                authorities: ['ROLE_USER']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/addemployee/addemployee-dialog.html',
+                    controller: 'AddemployeeDialogController',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: function () {
+                            return {
+                                empid: null,
+                                empname: null,
+                                department: null,
+                                designation: null,
+                                mobilenumber: null,
+                                emailid: null,
+                                dateofbirth: null,
+                                dateofjoining: null,
+                                relievingdate: null,
+                                active: null,
+                                createdate: null,
+                                updatedate: null,
+                                id: null
+                            };
+                        }
+                    }
+                }).result.then(function() {
+                    $state.go('addemployee', null, { reload: 'addemployee' });
+                }, function() {
+                    $state.go('addemployee');
+                });
+            }]
+        })
+
     }
+
 })();
