20deab184454f03544486c4b03fd9c427213442b
Raman <raman.kumar.rai@gmail.com>
2017-05-23 16:18:16 +0530

    client side validation for 10MB file upload in createfile module

--- src/main/webapp/app/services/FileManagement/FileManagement.directive.js
+++ src/main/webapp/app/services/FileManagement/FileManagement.directive.js
@@ -9,10 +9,23 @@
                     link: function(scope, element, attrs) {
                         var model = $parse(attrs.fileModel);
                         var modelSetter = model.assign;
+                        var maxSize = 10*1024*1024; //10 MB
 
                         element.bind('change', function(){
                             scope.$apply(function(){
+                            scope.file.maxSizeError = false;
+
+                            if (element[0].files.length > 1) {
+                                modelSetter(scope, element[0].files);
+                                } else {
                                 modelSetter(scope, element[0].files[0]);
+                                }
+                            var fileSize = element[0].files[0].size;
+                            if (fileSize > maxSize) {
+                                scope.file.maxSizeError = true;
+                            }
+
+
                             });
                         });
                     }
